8da8e10f091b85c1bf1c0095fce93e8f98d92ff9
JDavisWASD <jdavisWASD@gmail.com>
2021-12-03 14:59:43 -0800

    init

---
+++ README.txt
@@ -0,0 +1,43 @@
+A website that allows users to catalog their collection of Magic: The Gathering cards and see the average prices of said cards from 1-2 external websites via APIs.
+
+Landing Page:
+- Login and Register Buttons
+- Individual card search w/o login
+
+Search Result:
+- Individual card search w/o login
+- Display card result:
+    - Image of Card
+    - Card Name
+    - Card Description
+    - Prices
+
+Login Page:
+- Username and password input w/ validations
+
+Registration Page:
+- Username, email, password, and confirmation w/ validations/regex
+
+Collection Page:
+- Display total quantity of cards
+- Display number of unique cards
+- Display total price of all cards
+- Table of cards in collection
+    - Add cards
+    - Remove cards
+    - Edit quantity
+    - Clicking card name uses search result page to display full card info
+
+Data Plan:
+- Bulid a table of users as they register
+- Have a pre-built at table of existing cards and their information
+- Connect both tables in a many to many relationship based on user input
+
+Future Features:
+- Export/Import via CSV
+- Ability to subdivide a collection into multiple decks of cards
+- Password reset
+- Specify printing edition of cards
+- Sort by table column
+- Split table into pages
+- Make Login and Registartion popups/overlays instead of individual pages
---
+++ flask_app/config/mysqlconnection.py
@@ -0,0 +1,46 @@
+import pymysql.cursors
+
+class MySQLConnection:
+    def __init__(self, db):
+        connection = pymysql.connect(
+            host = 'localhost',
+            user = 'root',
+            password = 'root',
+            db = db,
+            charset = 'utf8mb4',
+            cursorclass = pymysql.cursors.DictCursor,
+            autocommit = True
+        )
+        self.connection = connection
+
+    def query_db(self, query, data = None):
+        with self.connection.cursor() as cursor:
+            try:
+                query = cursor.mogrify(query, data)
+                print("Running Query:", query)
+
+                cursor.execute(query, data)
+
+                #INSERT will return ID number of the inserted row
+                if query.lower().find("insert") >= 0:
+                    self.connection.commit()
+                    return cursor.lastrowid
+
+                #SELECT will return the data as a list of dictionaries
+                elif query.lower().find("select") >= 0:
+                    result = cursor.fetchall()
+                    return result
+
+                #UPDATE and DELETE will return nothing
+                else:
+                    self.connection.commit()
+
+            #If the query fails the method will return False
+            except Exception as error:
+                print("Something went wrong", error)
+                return false
+            finally:
+                self.connection.close()
+
+def connectToMySQL(db):
+    return MySQLConnection(db)
