e0bfa799c0dc01fa8b1de4f5626a6965128f9a2c
JDavisWASD <jdavisWASD@gmail.com>
2021-12-06 18:21:21 -0800

    basic card display

--- flask_app/controllers/main.py
+++ flask_app/controllers/main.py
@@ -9,12 +9,23 @@ def index():
 
 @app.route('/card')
 def displayCard():
-    return render_template('displayCard.html', searchResult = session['lastSearch'])
+    data = {
+        'image': session['lastSearch']['image_uris']['normal'],
+        'name': session['lastSearch']['name'],
+        'cost': session['lastSearch']['mana_cost'],
+        'type': session['lastSearch']['type_line'],
+        'description': session['lastSearch']['oracle_text']
+    }
+
+    if 'power' in session['lastSearch']:
+        data['power'] = session['lastSearch']['power']
+    if 'toughness' in session['lastSearch']:
+        data['toughness'] = session['lastSearch']['toughness']
+        
+    return render_template('displayCard.html', cardInfo = data)
 
 @app.route('/search', methods = ['POST'])
 def search():
     result = requests.get(f"https://api.scryfall.com/cards/named?exact={request.form['query']}")
     session['lastSearch'] = result.json()
-#    print(session['lastSearch']['object'])
-#    return redirect('/card')
-    return redirect('/')
+    return redirect('/card')
--- flask_app/static/script.js
+++
@@ -1,8 +0,0 @@
-function search(event) {
-    event.preventDefault()
-    var searchForm = document.getElementById('searchForm')
-    var form = new FormData(searchForm)
-    fetch('http://localhost:5000/search', {method: 'POST', body: form})
-        .then(res => res.json())
-        .then(data => console.log(data))
-}
