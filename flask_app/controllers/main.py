d5c4fa3218cea58655be752a9b2540b6d1719cd0
JDavisWASD <jdavisWASD@gmail.com>
2021-12-06 21:08:21 -0800

    basic price display

--- flask_app/controllers/main.py
+++ flask_app/controllers/main.py
@@ -9,23 +9,35 @@ def index():
 
 @app.route('/card')
 def displayCard():
-    data = {
-        'image': session['lastSearch']['image_uris']['normal'],
-        'name': session['lastSearch']['name'],
-        'cost': session['lastSearch']['mana_cost'],
-        'type': session['lastSearch']['type_line'],
-        'description': session['lastSearch']['oracle_text']
-    }
-
-    if 'power' in session['lastSearch']:
-        data['power'] = session['lastSearch']['power']
-    if 'toughness' in session['lastSearch']:
-        data['toughness'] = session['lastSearch']['toughness']
-        
-    return render_template('displayCard.html', cardInfo = data)
+    return render_template('displayCard.html', cardInfo = session['lastSearch'])
 
 @app.route('/search', methods = ['POST'])
 def search():
-    result = requests.get(f"https://api.scryfall.com/cards/named?exact={request.form['query']}")
-    session['lastSearch'] = result.json()
+    result = requests.get(f"https://api.scryfall.com/cards/named?exact={request.form['name']}&set={request.form['set']}")
+    result = result.json()
+    
+    print(result['oracle_text'])
+
+    data = {
+        'image': result['image_uris']['normal'],
+        'name': result['name'],
+        'cost': result['mana_cost'],
+        'type': result['type_line'],
+        'description': result['oracle_text'],
+        'prices' : {
+            'usd': result['prices']['usd'],
+            'usd_foil': result['prices']['usd_foil'],
+            'usd_etched': result['prices']['usd_etched'],
+            'eur': result['prices']['eur'],
+            'eur_foil': result['prices']['eur'],
+            'tix': result['prices']['tix']
+        }
+    }
+
+    if 'power' in result:
+        data['power'] = result['power']
+    if 'toughness' in result:
+        data['toughness'] = result['toughness']
+    
+    session['lastSearch'] = data
     return redirect('/card')
